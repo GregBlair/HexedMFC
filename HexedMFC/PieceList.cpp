50c9a3d0c662198b521ce637e2c68992b3d709b6
Greg Blair <gregblair@msn.com>
2025-06-06 09:56:20 -0500

    better but still buggy piece logic

--- HexedMFC/PieceList.cpp
+++ HexedMFC/PieceList.cpp
@@ -23,50 +23,41 @@ bool PieceList::SetBlockCount(size_t blockCount)
 
 void PieceList::GeneratePieceList(size_t blockCount)
 {
+    // Just one seed location should do it.
+    size_t cooridinateValue = blockCount / 2;
+    Offset seedOffset(cooridinateValue, cooridinateValue);
+    OffsetList offsets{ seedOffset };
+    NextOffset(offsets);
+    size_t firstTry = m_pieceList.size();
+    m_pieceList.clear();
     // Try all possible offset lists for the block count.
     for (int x = 0; x < blockCount / 2; ++x)
     {
-        Offset seedOffset(x, x);
+        Offset seedOffset(cooridinateValue, cooridinateValue);
         OffsetList offsets{ seedOffset };
-        NextOffsetGlobs(offsets, seedOffset);
+        NextOffset(offsets);
      }
+    size_t secondTry = m_pieceList.size();
+    ASSERT(firstTry == secondTry);
 }
 
-void PieceList::NextOffsetGlobs(OffsetList offsets, Offset lastOffset)
+void PieceList::NextOffset(OffsetList offsets)
 {
     OffsetList addedOffsets;
     // Try all directions.
-    // Right
-    if (lastOffset.first < m_blockCount)
-    {
-        HandleNextOffset(lastOffset, Direction::Right, offsets, addedOffsets);
-    }
-    // Left
-    if (lastOffset.first > 0)
-    {
-        HandleNextOffset(lastOffset, Direction::Left, offsets, addedOffsets);
-    }
-    // Up
-    if (lastOffset.second < m_blockCount)
-    {
-        HandleNextOffset(lastOffset, Direction::Up, offsets, addedOffsets);
-    }
-    // Down
-    if (lastOffset.second > 0)
-    {
-        HandleNextOffset(lastOffset, Direction::Down, offsets, addedOffsets);
-    }
+    HandleNextOffset(Direction::Right, offsets, addedOffsets);
+    HandleNextOffset(Direction::Left, offsets, addedOffsets);
+    HandleNextOffset(Direction::Up, offsets, addedOffsets);
+    HandleNextOffset(Direction::Down, offsets, addedOffsets);
 
-    // Need to try all possible combinations of the offsets added at this level.
-    std::list<OffsetList> combos = GenerateViableCombos(addedOffsets);
-
-    for (OffsetList& offsetList : combos)
+    if (addedOffsets.size() > 0)
     {
-        size_t count = offsetList.size();
-        for (Offset& offset : offsetList)
+        // Need to try all possible combinations of the offsets added at this level.
+        std::list<OffsetList> combos = GenerateViableCombos(addedOffsets);
+
+        for (const OffsetList& offsetList : combos)
         {
-            // Add the offsets to the current list and try again.
-            // Copy the offset lists so they can be reused.
+            // Add the entire offset list and try it.
             OffsetList existingOffsetListCopy = offsets;
             OffsetList newOffsetListCopy = offsetList;
             existingOffsetListCopy.splice(existingOffsetListCopy.end(), newOffsetListCopy);
@@ -76,7 +67,7 @@ void PieceList::NextOffsetGlobs(OffsetList offsets, Offset lastOffset)
             }
             else if (existingOffsetListCopy.size() < m_blockCount)
             {
-                NextOffsetGlobs(existingOffsetListCopy, existingOffsetListCopy.back());
+                NextOffset(existingOffsetListCopy);
             }
         }
     }
@@ -91,6 +82,7 @@ void PieceList::TryOffsets(OffsetList& offsets)
         if (piece.isEquivalent(offsets))
         {
             found = true;
+            break;
         }
     }
 
@@ -101,26 +93,53 @@ void PieceList::TryOffsets(OffsetList& offsets)
 }
 
 // Returns the count of blocks added, 1 or 0.
-void PieceList::HandleNextOffset(const Offset& lastOffset, Direction direction, OffsetList offsets, OffsetList& addedOffsets)
+void PieceList::HandleNextOffset(Direction direction, OffsetList offsets, OffsetList& addedOffsets)
 {
+    Offset lastOffset = offsets.back();
     Offset nextOffset;
+    bool validDirection = false;
     switch (direction)
     {
-    case Up:
-        nextOffset = Offset(lastOffset.first, lastOffset.second + 1);
-        break;
     case Right:
-        nextOffset = Offset(lastOffset.first + 1, lastOffset.second);
-        break;
-    case Down:
-        nextOffset = Offset(lastOffset.first, lastOffset.second - 1);
-        break;
-    case Left:
-        nextOffset = Offset(lastOffset.first - 1, lastOffset.second);
+    {
+        if (lastOffset.first < m_blockCount)
+        {
+            nextOffset = Offset(lastOffset.first + 1, lastOffset.second);
+            validDirection = true;
+        }
         break;
     }
-    // If the offset is not already occupied.
-    if (std::find(offsets.begin(), offsets.end(), nextOffset) == offsets.end())
+    case Left:
+    {
+        if (lastOffset.first > 0)
+        {
+            nextOffset = Offset(lastOffset.first - 1, lastOffset.second);
+            validDirection = true;
+        }
+        break;
+    }
+    case Up:
+    {
+        if (lastOffset.second < m_blockCount)
+        {
+            nextOffset = Offset(lastOffset.first, lastOffset.second + 1);
+            validDirection = true;
+        }
+        break;
+    }
+    case Down:
+    {
+        if (lastOffset.second > 0)
+        {
+            nextOffset = Offset(lastOffset.first, lastOffset.second - 1);
+            validDirection = true;
+        }
+        break;
+    }
+    }
+    // If the offset is a valid direction and is not already occupied.
+    if (validDirection &&
+        std::find(offsets.begin(), offsets.end(), nextOffset) == offsets.end())
     {
         offsets.push_back(nextOffset);
         if (offsets.size() == m_blockCount)
@@ -130,7 +149,7 @@ void PieceList::HandleNextOffset(const Offset& lastOffset, Direction direction, 
         }
         else
         {
-            NextOffsetGlobs(offsets, nextOffset);
+            NextOffset(offsets);
             addedOffsets.push_back(nextOffset);
         }
     }
@@ -140,21 +159,36 @@ std::list<OffsetList> PieceList::GenerateViableCombos(const OffsetList& addedOff
 {
     std::list<OffsetList> retVal;
     // Create two list copies and start the recursion.
-    OffsetList original = addedOffsets;
-    OffsetList transfered;
-    AddToCombinations(retVal, original, transfered);
+    std::list<OffsetList> transfered;
+    AddToCombinations(retVal, OffsetList(addedOffsets));
     return retVal;
 }
 
-void PieceList::AddToCombinations(std::list<OffsetList>& listOfLists, const OffsetList& original, const OffsetList& transfered)
+std::list<OffsetList> PieceList::AddToCombinations(std::list<OffsetList>& results, OffsetList right)
 {
-    for (Offset offset : original)
+    results.push_back(right);
+    std::list<OffsetList> retVal;
+    // Add each of the transferred lists to each of the subsequent lists.
+    if (right.size() > 1)
     {
-        OffsetList originalCopy = original;
-        OffsetList transferedCopy = transfered;
-        auto iter = std::find(originalCopy.begin(), originalCopy.end(), offset);
-        transferedCopy.splice(transferedCopy.end(), originalCopy, iter);
-        listOfLists.push_back(transferedCopy);
-        AddToCombinations(listOfLists, originalCopy, transferedCopy);
+        OffsetList left;
+        // Move one element from right to left
+        left.splice(left.end(), right, right.begin());
+        // Add both lists to the results.
+        results.push_back(left);
+        results.push_back(right);
+        retVal.push_back(left);
+        // Add the left offset to every item from the next recursive call.
+        // Add each transferred list to every subsequent list.
+        for (OffsetList& offsetList : AddToCombinations(results, right))
+        {
+            OffsetList temp(left);
+            // Put the left together with every result from the subsequent calls;
+            temp.insert(temp.end(), offsetList.begin(), offsetList.end());
+            results.push_back(temp);
+            retVal.push_back(temp);
+        }
     }
+
+    return retVal;
 }
